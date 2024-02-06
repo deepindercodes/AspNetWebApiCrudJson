e52463ad8a2f7df9edf5de3dcf604b0928f16cb4
Deepinder Singh <deepindersingh006@gmail.com>
2024-02-06 15:08:12 +0530

    Add project files.

---
+++ Controllers/ArticlesController.cs
@@ -0,0 +1,47 @@
+﻿using AspNetWebApiCrudJson.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace AspNetWebApiCrudJson.Controllers
+{
+    public class ArticlesController : ApiController
+    {
+        jsonData objarticles = new jsonData();
+        
+        // GET: api/Articles
+        public IEnumerable<Article> Get()
+        {
+            return objarticles.GetArticles();
+        }
+
+        // GET api/values/5
+        public Article Get(int id)
+        {
+            return objarticles.GetArticles().Where(u => u.id == id.ToString()).FirstOrDefault();
+        }
+
+        // POST: api/Articles
+        [HttpPost]
+        public string Post([FromBody]Article article)
+        {
+            objarticles.AddArticle(article);
+            return "{ status : 'New Article Added'}";
+        }
+
+        // PUT: api/Articles/5
+        public string Put(int id, [FromBody] Article article)
+        {
+            objarticles.EditArticle(article, id);
+            return "{ status : 'Article Edited'}";
+        }
+
+        // DELETE: api/Articles/5
+        public string Delete(int id)
+        {
+            objarticles.DeleteArticle(id.ToString());
+            return "{ status : 'Article Deleted'}";
+        }
+    }
+}
